783e05d850d2bef8543ad23ba5e94a8d86bbd979
divband <divband@ava-telecom.ir>
2019-12-05 05:19:19 +0330

    client chatroom created

---
+++ chatRoom/clientChatroom.py
@@ -0,0 +1,43 @@
+import socket
+import time
+import sys
+import threading
+
+IP = 'localhost'
+PORT = 8216
+username = input("Enter your name: ")
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((IP, PORT))
+client_socket.setblocking(0)
+
+
+def send_message():
+    while True:
+        msg = input()
+        if msg:
+            client_socket.send(bytes(username + "->" + msg, 'utf-8'))
+
+
+t1 = threading.Thread(target=send_message)
+t1.start()
+
+
+while True:
+    # #    message = client_socket.recv(1024)
+    # #    print(message.decode('utf-8'))
+    # msg = input('{}-> '.format(username))
+    # if msg:
+    #     client_socket.send(bytes(username + "->" + msg, 'utf-8'))
+
+    try:
+        while True:
+            message = client_socket.recv(1024)
+            if not message:
+                print("Connection Closed!")
+                sys.exit()
+            print(message.decode('utf-8'))
+
+    except IOError as e:
+        pass
+
+#    time.sleep(5)
